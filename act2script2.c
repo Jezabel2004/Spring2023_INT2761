4f544669f19da69b80678af8cac632a20a66ab84
Jezabel2004 <125329012+Jezabel2004@users.noreply.github.com>
2023-02-26 10:45:17 -0500

    Add files via upload

---
+++ act2script2.c
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Enter First and Last Name"
+read VAR
+
+echo "Enter Student ID"
+read VAR
+
+if {{ $VAR = 123}}
+
+then
+
+echo “Welcome [first and last name], welcome to your virtual machine!"
+
+else 
+
+echo "Error, invalid StudentID: [student ID].  Please try again."
+
